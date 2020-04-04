2c7a32b44017a1aa4c7c3fdae9d0e651feab4b13
rizalbayyu <38995446+rizalbayyu@users.noreply.github.com>
2020-04-04 19:07:38 +0700

    Update serverrcvfile.py

--- socketTransferFile/serverrcvfile.py
+++ socketTransferFile/serverrcvfile.py
@@ -5,21 +5,22 @@ s.bind(("127.0.0.1", 7501))
 s.listen(5)   
 
 try:
-
     while True:
         conn, address = s.accept()
         print (f"Got connection from {address}")
-        i=1
-        file = open('test_file_'+ str(i)+".pdf",'wb')
-        i=i+1
+        file = open("test_file_1.pdf","wb") # Write on binary mode
         while (True):       
     # receive data and write it to file
-            recvdata = conn.recv(1024)
+            recvdata = conn.recv(4)
             while (recvdata):
-                file.write(recvdata)
-                recvdata = conn.recv(1024)
-        file.close()
-        conn.close()
+                recvdata = conn.recv(4) #Trying to receive data again from client if thereis another data.
+                if not recvdata:
+                    file.close()
+                    break
+                else:
+                    file.write(recvdata) #write data to test_file_1.pdf
+
 except KeyboardInterrupt:
-    print("Server Down, check file on the directory")
     s.close()
+    print()
+    print("Server Down, check file on the directory")
