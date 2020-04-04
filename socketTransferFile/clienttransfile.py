11adaf0a6b9eaa8d6e558291e13008fdb6ebf965
rizalbayyu <38995446+rizalbayyu@users.noreply.github.com>
2020-04-04 19:06:49 +0700

    Update clienttransfile.py

--- socketTransferFile/clienttransfile.py
+++ socketTransferFile/clienttransfile.py
@@ -1,13 +1,13 @@
-import socket
-# import sys        
+import socket       
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           
 s.connect(("127.0.0.1", 7501))
 
-filename = "test1.pdf"
-file = open(filename, "rb")
-readfile = file.read(1024)
+filename = "test.pdf"
+file = open(filename, "rb") #Read on binary mode
+readfile = file.read(4) #Put data on file into readfile variable
 while (readfile):
-    s.send(readfile)
-    readfile = file.read(1024)
+    s.send(readfile) #Send data
+    readfile = file.read(4) #Read data again on the file
+print("Berhasil kirim data")
 s.close()
